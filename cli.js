6e16d83bafc8c39a55a17045c948ae7644b8b074
cannibalox <cannibalox@users.noreply.github.com>
2023-01-06 19:57:06 +0100

    add cli-select and clipboardy - enables selection via a list of results

--- cli.js
+++ cli.js
@@ -1,103 +1,187 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-import program from 'commander';
-import clipboardy from 'clipboardy';
-import chalk from 'chalk';
-import elegantSpinner from 'elegant-spinner';
-import logUpdate from 'log-update';
-import fetch from 'isomorphic-fetch';
+const program = require("commander");
+const chalk = require("chalk");
+const elegantSpinner = require("elegant-spinner");
+const logUpdate = require("log-update");
+const fetch = require("isomorphic-fetch");
+const Promise = require("es6-promise").Promise;
 const frame = elegantSpinner();
+const cliSelect = require("cli-select");
+const clipboardy = require("node-clipboardy");
 
 const propsToShow = [
-  'Year', 'Genre', 'Director', 'Writer', 'Actors',
-  'Plot', 'Language', 'Country', 'Awards', 
-  'Metascore', 'imdbRating', 'Production', 'Released', 'Runtime', 'BoxOffice', 'Poster'
-]; // 'Title'
-  
-const propsToCompare = [
-  'Title', 'Year', 'Released', 'Runtime',
-  'Genre', 'Metascore', 'imdbRating', 'tomatoMeter',
-  'BoxOffice', 'Production'
+  "Type",
+  "Year",
+  "Genre",
+  "Director",
+  "Writer",
+  "Actors",
+  "Plot",
+  "Language",
+  "Country",
+  "Awards",
+  "Metascore",
+  "imdbRating",
+  "Production",
+  "Released",
+  "Runtime",
+  "BoxOffice",
+  "imdbID",
+  "Poster",
 ];
+// Title, Rated,imdbVotes, Type, DVD, Production
+const propsToCompare = [
+  "Title",
+  "Year",
+  "Released",
+  "Runtime",
+  "Genre",
+  "Metascore",
+  "imdbRating",
+  "BoxOffice",
+  "Production",
+  "Director",
+  "Writer",
+  "Actors",
+];
+const defaultKey = "5e540903";
 
-const defaultKey = '5e540903';
+const initialUrl = "http://www.omdbapi.com/?apikey=";
 
-const initialUrl = 'http://www.omdbapi.com/?apikey=';
-  
 program
-.description('Get information about a movie or tv series or compare two movies!')
-.parse(process.argv);
+  .description(
+    "Get information about a movie or tv series or compare two movies!"
+  )
+  .parse(process.argv);
 
-if(program.args.length < 1) {
-  console.log(chalk.red('Please give a movie name!!')); 
+if (program.args.length < 1) {
+  console.log(chalk.red("Please give a movie name!!"));
   process.exit(1);
 }
 
-if(program.args.join().toUpperCase().indexOf('::') !== -1) {
-  const interval1 = setInterval(function() {
-  logUpdate("Loading..." + chalk.cyan.bold.dim(frame()));
-  }, 50)
+if (program.args.join().toUpperCase().indexOf("::") !== -1) {
   const movies = program.args.join(" ").toUpperCase().split("::");
-  const urls = movies.map(function(mov) {
-    return `${initialUrl}${defaultKey}&t='${mov.trim().replace(/ /g,"+")}`
+  const urls = movies.map(function (mov) {
+    return `${initialUrl}${defaultKey}&t=${mov.trim().replace(/ /g, "+")}`;
   });
-  
-  Promise.all(urls.map(fetch)).
-  then(function(res) { return Promise.all(res.map(function(res) { return res.json()})) }).
-  then(function(movies) { 
-    clearInterval(interval1); 
-    logUpdate.clear();
-    compareInfo(movies) 
+  //console.log('urls0 : ', urls[0]," >>> url1: ",urls[1]);
+  const m0 = fetchMovie(urls[0]);
+  const m1 = fetchMovie(urls[1]);
+  Promise.all([m0, m1]).then((movies) => {
+    compareInfo(movies);
   });
-  }
-else {
-  const interval = setInterval(function() {
-  logUpdate("Loading..." + chalk.cyan.bold.dim(frame()));
-  }, 50)
-  fetch(`${initialUrl}${defaultKey}&t=${program.args.join().trim().replace(/ /g,"+")}`)
-  .then(function(res) { return res.json()})
-  .then(function(mov) {
-    clearInterval(interval); 
-    logUpdate.clear();
-    printInfo(mov)});
+} else {
+  const interval = setInterval(function () {
+    logUpdate("Loading..." + chalk.cyan.bold.dim(frame()));
+  }, 50);
+  fetch(
+    `${initialUrl}${defaultKey}&s=${program.args
+      .join()
+      .trim()
+      .replace(/ /g, "+")}`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (mov) {
+      clearInterval(interval);
+      logUpdate.clear();
+      printInfo(mov);
+    });
+}
+
+async function fetchMovie(url) {
+  const interval1 = setInterval(function () {
+    logUpdate("Loading..." + chalk.cyan.bold.dim(frame()));
+  }, 50);
+  const res = await fetch(url);
+  const metadata = await res.json();
+  clearInterval(interval1);
+  logUpdate.clear();
+  return metadata;
 }
 
 function compareInfo(movies) {
-  if(movies[0].Response === 'False' || movies[1].Response === 'False') {
-    console.log(chalk.red('Movie not found!'));
+  if (movies[0].Response === "False" || movies[1].Response === "False") {
+    console.log(chalk.red("Movie not found!"));
     process.exit(1);
   }
-  
-  propsToCompare.forEach(function(prop, i, arr) {
-        if(movies[0][prop] === 'N/A' && movies[1][prop] === 'N/A') {
-          return ;
-        }
-        console.log(chalk.bold.cyan(prop), " ".repeat(13-prop.length), movies[0][prop], "", 
-        " ".repeat(50-movies[0][prop].length), movies[1][prop], ""
-        );
+
+  propsToCompare.forEach(function (prop, i, arr) {
+    if (movies[0][prop] === "N/A" && movies[1][prop] === "N/A") {
+      return;
+    }
+    console.log(
+      chalk.bold.cyan(prop),
+      " ".repeat(13 - prop.length),
+      movies[0][prop],
+      "",
+      " ".repeat(50 - movies[0][prop].length),
+      movies[1][prop],
+      ""
+    );
   });
-  
 }
 
 function printInfo(movie) {
-  if(movie.Response === 'False') {
-    console.log(chalk.red(movie.Error));
+  if (movie.Response === "False") {
+    console.log(chalk.red("No matching titles found!"));
     process.exit(1);
   }
-  var output = "Tags:: #movies, #watched\n"; // hpx
-  propsToShow.forEach(function(prop, i, arr) {
-        if(movie[prop] !== 'N/A'){
-        console.log(chalk.bold.cyan(prop)+"::", movie[prop], "");
-
-        // === coopy to clipboard hpx====
-        if (prop == 'Plot' || prop == 'BoxOffice' || prop == 'Awards') {output = output.concat(prop,':: "', movie[prop], '"\n');}
-          else
-        {output = output.concat(prop,":: ", movie[prop], "\n");} //hpx
-        }
-  });
-// copy to clipboard
-clipboardy.writeSync(output);
-console.log('\n>>>>>>  copied to clipboard ! <<<<<<<<<<<<<');
+  const movielist = new Array();
+  for (const result of movie["Search"]) {
+    movielist.push(result["Title"]);
+  }
+  cliSelect({
+    values: movielist,
+    cleanup: true,
+    indentation: 3,
+    valueRenderer: (value, selected) => {
+      if (selected) {
+        return chalk.bold.green.underline(value);
+      }
+      return value;
+    },
+  })
+    .then((value) => {
+      console.log(
+        chalk.bold.green.underline(
+          "\n" + value.value + " (" + movie["Search"][value.id]["Year"] + ")"
+        )
+      );
+      const interval = setInterval(function () {
+        logUpdate("Loading..." + chalk.cyan.bold.dim(frame()));
+      }, 50);
+      fetch(`${initialUrl}${defaultKey}&plot=full&t=${value.value}`)
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (movie) {
+          clearInterval(interval);
+          logUpdate.clear();
+          if (movie.Response === "False") {
+            console.log(chalk.red(movie.Error));
+            process.exit(1);
+          }
+          var output = "Tags:: #movies, #watched\n";
+          propsToShow.forEach(function (prop, i, arr) {
+            if (movie[prop] !== "N/A") {
+              console.log(chalk.bold.cyan(prop) + "::", movie[prop], "");
+              // === copy to clipboard ====
+              if (prop == "Plot" || prop == "BoxOffice" || prop == "Awards") {
+                output = output.concat(prop, ':: "', movie[prop], '"\n');
+              } else {
+                output = output.concat(prop, ":: ", movie[prop], "\n");
+              }
+            }
+          });
+          clipboardy.writeSync(output);
+          console.log("\n>>> metadata copied to clipboard ! <<<\n\n");
+        });
+    })
+    .catch(() => {
+      console.log("cancelled");
+    });
 }
-
