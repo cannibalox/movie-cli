d2c532346409efcd42e3a4516757856e6750e191
cannibalox <cannibalox@users.noreply.github.com>
2023-01-14 20:07:04 +0100

    cfg

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 export const cfg = {
   // enter your api keys (enclosed in brackets) here
-  omdbapiKey: "xxxxxxxx",
-  tmdbKey: "xxxxxxxx", // req for Original-Title property
+  omdbapiKey: "5e540903",
+  tmdbKey: "", // req for Original-Title property
 
   // general options
   saveFilePath: "d:\\ls-test\\pages\\", // needs to be a valid filepath with trailing salash
-  copyToClipboard: true,                // true|false
-  includeTitleProp: true,               // add `title:: movietitle [[year]]`
-  DefaultTags: "#watched",              // add custom tags property `tags:: #watched`
-  addCustomInfo: true,                  // if `false` it only displays info and exits
-    askRating: true,                    // add custoôrating 1-10 (that gets converted to stars)
-    askWatchedDate: true,               // add a date formatted as myDateFormat on output
-    askAnything: false,                 // input any text/string
-    pathToJournal: "d:\\ls-test\\journals\\",
-    mytemplate: "- DONE #watched [[%movietitle%]]", //text that will be appended to daily journam 
-  encloseStrings: true,                 // enclose value strings in doublébrackets.
-                                        // for Plot, BoxOffice, Awards
-                                        // if true `plot:: "foo, bar"` else `plot:: foo,bar`
-  myDateFormat: "yyyymmdd",             // see https://github.com/felixge/node-dateformat#mask-options
-  fetchOriginalTitle: true,             // add original title property. requires TMDB api key`
+  copyToClipboard: true, // true|false
+  includeTitleProp: true, // add `title:: movietitle [[year]]`
+  DefaultTags: "#watched", // add custom tags property `tags:: #watched`
+  addCustomInfo: true, // if `false` it only displays info and exits
+  askRating: true, // add custoôrating 1-10 (that gets converted to stars)
+  askWatchedDate: true, // add a date formatted as myDateFormat on output
+  askAnything: false, // input any text/string
+  pathToJournal: "d:\\ls-test\\journals\\",
+  mytemplate: "- DONE #watched [[%movietitle%]]", //text that will be appended to daily journam
+  encloseStrings: true, // enclose value strings in doublébrackets.
+  // for Plot, BoxOffice, Awards
+  // if true `plot:: "foo, bar"` else `plot:: foo,bar`
+  myDateFormat: "yyyymmdd", // see https://github.com/felixge/node-dateformat#mask-options
+  fetchOriginalTitle: true, // add original title property. requires TMDB api key`
 
   // define properties to fetch for movie details
   propsToShow: [
     //  "Title"
     //  "Type",
-    "original-title",                   // is a special case handled by TMDB
+    "original-title", // is a special case handled by TMDB
     "Year",
     "Genre",
     "Director",
